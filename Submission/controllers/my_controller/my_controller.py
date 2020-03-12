42315cddbac82a99a237ccf087c1769caf0fffc0
zenongreat <jpowell6@illinois.edu>
2020-03-11 19:55:58 -0500

    Figured out controller directory.

--- Submission/controllers/my_controller/my_controller.py
+++ Submission/controllers/my_controller/my_controller.py
@@ -1,56 +1,12 @@
-"""my_controller controller."""
-
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
 from controller import Robot, Motor, DistanceSensor
 
-TIME_STEP = 64
-
-MAX_SPEED = 6.28
-
-# create the Robot instance.
 robot = Robot()
 
-sensors = []
-
-sensor_names = ['ps0', 'ps1', 'ps2', 'ps3', 'ps4', 'ps5', 'ps6', 'ps7']
-
-for i in range(8):
-    sensors.append(robot.getDistanceSensor(sensor_names[i]))
-    sensors[i].enable(TIME_STEP)
-
-leftMotor = robot.getMotor('left wheel motor')
-rightMotor = robot.getMotor('right wheel motor')
-leftMotor.setPosition(float('inf'))
-rightMotor.setPosition(float('inf'))
-leftMotor.setVelocity(0.0)
-rightMotor.setVelocity(0.0)
-
-while robot.step(TIME_STEP) != -1:
-    sensor_values = []
-    for i in range(8):
-        sensor_values.append(sensors[i].getValue())
-        print("sensor value from " + sensor_names[i] + " is ")
-        print(sensor_values[i])
-   
-    # detect obstacles
-    right_obstacle = sensor_values[0] > 100.0 or sensor_values[1] > 100.0 or sensor_values[2] > 100.0
-    left_obstacle = sensor_values[5] > 100.0 or sensor_values[6] > 100.0 or sensor_values[7] > 100.0
-
-    # initialize motor speeds at 50% of MAX_SPEED.
-    leftSpeed  = 0.5 * MAX_SPEED
-    rightSpeed = 0.5 * MAX_SPEED
-    # modify speeds according to obstacles
-    if left_obstacle:
-        # turn right
-        leftSpeed  += 0.5 * MAX_SPEED
-        rightSpeed -= 0.5 * MAX_SPEED
-    elif right_obstacle:
-        # turn left
-        leftSpeed  -= 0.5 * MAX_SPEED
-        rightSpeed += 0.5 * MAX_SPEED
-    # write actuators inputs
-    leftMotor.setVelocity(leftSpeed)
-    rightMotor.setVelocity(rightSpeed)
-   
-    pass
+# initialize motors
+mot = []
+motNames = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
+for name in motNames:
+    mot.append(robot.getMotor(name))
+    print("Got " + name)
+    
+pass
