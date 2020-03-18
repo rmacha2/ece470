1adc0dbd4e275d40a7b0649cf3d96b03cb00ed79
zenongreat <jpowell6@illinois.edu>
2020-03-18 11:00:19 -0500

    While not workin

--- Submission/controllers/my_controller/my_controller.py
+++ Submission/controllers/my_controller/my_controller.py
@@ -1,4 +1,5 @@
 from controller import Robot, Motor, DistanceSensor
+import time
 
 robot = Robot()
 
@@ -8,5 +9,17 @@ motNames = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1
 for name in motNames:
     mot.append(robot.getMotor(name))
     print("Got " + name)
-    
+
+dir = 0
+
+while(True):
+    if dir == 0:
+        mot[1].setVelocity(1.5)
+        mot[1].setPosition(-3.14)
+        dir = 1
+    if dir == 1:
+        mot[1].setVelocity(1.5)
+        mot[1].setPosition(3.14)
+        dir = 0
+
 pass
