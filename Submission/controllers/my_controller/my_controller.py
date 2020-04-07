f683a65e6ac73b7e1456791d9b0c96462268a8a8
ricky <rmacha2@illinois.edu>
2020-04-06 21:49:56 -0500

    Added a camera

--- Submission/controllers/my_controller/my_controller.py
+++ Submission/controllers/my_controller/my_controller.py
@@ -2,10 +2,12 @@ from controller import Robot, Motor, DistanceSensor
 from ikpy.chain import Chain
 import numpy as np
 import time
-
+from controller import Camera, Device
 
 
 robot = Robot()
+
+
 
 # initialize motors
 mot = []
