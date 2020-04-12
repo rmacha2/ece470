c922b6fff5e60e6ce78bbf85f173dcde279d7274
ricky <rmacha2@illinois.edu>
2020-04-12 14:04:41 -0500

    inverse kimatics figure out

--- Submission/controllers/my_controller/my_controller.py
+++ Submission/controllers/my_controller/my_controller.py
@@ -1,5 +1,6 @@
 from controller import Robot, Motor, DistanceSensor
 from ikpy.chain import Chain
+import ikpy
 import numpy as np
 import time
 
@@ -18,7 +19,16 @@ my_chain = Chain.from_urdf_file("../../ur10.urdf")
 
 a = np.array([6.0,-0.5,0.0,0.0,0.0,0.0,0.0,7.0])
 b = np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
-z = 0
+
+temo = np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+z = 1
+
+T1 = np.array([[0.0,0.0,0.0,1.5],\
+                [0.0,0.0,0.0,1.0],\
+                [0.0,0.0,0.0,2.0],\
+                [0.0,0.0,0.0,1.0]])
+                
+tempAngles = []
 
 while 1:  
     if z == 1:
@@ -29,21 +39,29 @@ while 1:
             i +=1
         
         ret = my_chain.forward_kinematics(b, True)
+       # print(ret)
         
+       # print("help")
         j = 1
         for item in ret:
             if j == 1:
-                print("origin is")
+             #   print("origin is")
+             x = 2
             elif j == 8:
-                print("end is")
+              #  print("end is")
+              x = 2
             else:
-                print("joint " + str(j -1 ) + " is ")
+               # print("joint " + str(j -1 ) + " is ")
+               x = 2
             print(item)
             j += 1
             #function v
-            inv = my_chain.inverse_kinematics(ret[7], 0)
-            print(inv[0])
+
             #function ^
+        print(ret[7])
+        print("helpy helpy")
+        inv = ikpy.inverse_kinematics.inverse_kinematic_optimization(chain = my_chain, target_frame = ret[7], starting_nodes_angles = b)
+        print(inv)
         robot.step(16000) 
         z = 0
         
